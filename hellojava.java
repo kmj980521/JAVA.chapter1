1c06b047f9d735bba9703956de131910ad6d7f91
kmj980521 <61898890+kmj980521@users.noreply.github.com>
2020-03-09 16:35:02 +0900

    Update hellojava.java

--- hellojava.java
+++ hellojava.java
@@ -3,10 +3,8 @@
 public class Program {
 
 		public static void main(String[] args) {
-					System.out.println("처음 작성하는 Java프로그램");
-							System.out.println("화면으로 출력하고 있습니다.");
-
-								}
-
+				System.out.println("처음 작성하는 Java프로그램");
+				System.out.println("화면으로 출력하고 있습니다.");
+							}
 }
 

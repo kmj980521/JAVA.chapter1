bcaf8de9faa7c445ce1e0c07ee2e9b5c0798f02f
MinJae <guswlfahs123@naver.com>
2020-03-09 16:56:05 +0900

    HowAreYou2.java

---
+++ HowAreYou2.java
@@ -0,0 +1,11 @@
+package ex1.io;
+
+public class HowAreYou1 {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요!"+"건강은 어떠세요?");//+사용
+		System.out.println("안녕하세요! \n건강은 어떠세요?");//줄바꿈\n사용
+	}
+
+}
+

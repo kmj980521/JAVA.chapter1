08f48a5fb1cf8499e6fe06956cac23b75374c26e
MinJae <guswlfahs123@naver.com>
2020-03-09 16:34:27 +0900

    hellojava.java

---
+++ hellojava.java
@@ -0,0 +1,12 @@
+[200~package ex1.io;
+
+public class Program {
+
+		public static void main(String[] args) {
+					System.out.println("처음 작성하는 Java프로그램");
+							System.out.println("화면으로 출력하고 있습니다.");
+
+								}
+
+}
+
